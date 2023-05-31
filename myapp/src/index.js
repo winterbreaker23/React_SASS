f86a4f1fcd3692599f5892f78bcc3e21f0533a8a
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 21:24:13 +0100

    Sass

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style.scss';
 
+const Header = () => {
+  return (
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+    </>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Header/>
 );
 
 // If you want to start measuring performance in your app, pass a function
